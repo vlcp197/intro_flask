e23e8609d3d99649a8c28861d40eb6384748f32f
vlcp197 <vlcp197@gmail.com>
2022-06-08 10:29:49 -0300

    Inserção de login e logout

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,14 @@
-from flask import render_template
-from app import app, db
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user
+from app import app, db, lm
 
 from app.models.forms import LoginForm
 from app.models.tables import User 
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 @app.route('/index')
 @app.route('/')
@@ -10,18 +16,23 @@ def index():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET','POST'])
+
 def login():
     form =  LoginForm()
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            return redirect(url_for("index"))
+            flash('Logged in.')
+        else:
+            flash('Invalid login.')
     else:
         print(form.errors)
     return render_template('login.html',form=form)
 
-@app.route("/teste/<info>")
-@app.route("/teste", defaults={'info': ''})
-def teste(info):
-    r = User.query.filter_by(username="teste").all()
-    print(r)
-    return "Ok"
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("Logged out.")
+    return redirect(url_for("index"))
